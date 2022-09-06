455c1918a417c82a40b1ed1c78f7677650e3e6ce
Jiselle Havas <jiselle.havas@duke.edu>
2022-09-06 18:39:37 -0400

    Check input value of cholesterol with criteria

--- total_cholesterol.py
+++ total_cholesterol.py
@@ -9,3 +9,11 @@ Created on Tue Sep  6 18:33:06 2022
 def input_chol():
     chol_input = input("Enter the total cholesterol value:")
     return int(chol_input)
+
+def check_chol(num_chol):
+    if num_chol >= 240:
+        return "high"
+    elif num_chol >= 200:
+        return "borderline high"
+    else:
+        return "normal"
