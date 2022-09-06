34af935a966f18f16f29fad54b8f57c786d6453c
Jiselle Havas <jiselle.havas@duke.edu>
2022-09-06 18:35:02 -0400

    Input cholesterol value

---
+++ total_cholesterol.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep  6 18:33:06 2022
+
+@author: jisellehavas
+"""
+
+def input_chol():
+    chol_input = input("Enter the total cholesterol value:")
+    return int(chol_input)
