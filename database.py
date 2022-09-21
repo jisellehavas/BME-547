6238ec4eea521ace9208e7b066ae1abfbdcfa5b6
Jiselle Havas <jiselle.havas@duke.edu>
2022-09-21 13:13:55 -0400

    fixed database pycodestyle

--- database.py
+++ database.py
@@ -5,13 +5,17 @@ Created on Wed Sep  7 12:11:56 2022
 
 @author: jisellehavas
 """
+import blood_calculator
+
 print("This is the database.py module")
 print("Python thinks this is called {}".format(__name__))
-import blood_calculator
-#from blood_calculator import check_HDL
-#import blood_calculator as bc
-#from blood_calculator import * (means import everything, if you have multiple of these then you won't know where it is coming from)
 
+"""
+from blood_calculator import check_HDL
+import blood_calculator as bc
+from blood_calculator import * (means import everything, 
+if you have multiple of these then you won't know where it is coming from)
+"""
 
 answer = blood_calculator.check_HDL(55)
 #answer = check_HDL(55)
