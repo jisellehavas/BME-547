e74b6b0e7039ac591d140cfed7a31ae79d7e5e4f
Jiselle Havas <jiselle.havas@duke.edu>
2022-09-21 14:53:57 -0400

    fixed test weight entry pycodestyle

--- test_weight_entry.py
+++ test_weight_entry.py
@@ -4,14 +4,14 @@ import pytest
 @pytest.mark.parametrize("input, expected", [
     ("22 lb", 10),
     ("50 kg", 50),
-    ("35.6 kg",36),
-    ("-22 lb",-10),
+    ("35.6 kg", 36),
+    ("-22 lb", -10),
     ("22 LB", 10),
     ("22 pounds", 10),
     ("22 lb, 50 kg", None),
-    ("22",None)
-    #("ten kg", 10)
-    ])
+    ("22", None)])
+
+
 def test_parse_weight_input(input, expected):
     from weight_entry import parse_weight_input
     answer = parse_weight_input(input)
